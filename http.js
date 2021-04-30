b4d0546aed7145c5d261f28612c4c3acd119a0c3
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-04-30 22:12:10 +0500

    updated

--- http.js
+++ http.js
@@ -3,13 +3,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/course" && req.method == "GET") {
+  if (req.url == "/user") {
+    if (req.method == "GET") {
+      res.write("User Fetched Successfully!");
+    } else if (req.method == "POST") {
+      res.write("User Created Successfully!");
+    } else if (req.method == "DELETE") {
+      res.write("User Deleted Successfully!");
+    }
+  } else if (req.url == "/course") {
+    if (req.method == "GET") {
+      res.write("Course Fetched Successfully!");
+    } else if (req.method == "POST") {
+      res.write("Course Created Successfully!");
+    } else if (req.method == "DELETE") {
+      res.write("Course Deleted Successfully!");
+    }
   }
-  res.statusCode = 201;
-  res.write("User Created Successfully!");
-  console.log(req.url, req.method);
-
   res.end();
+
+  res.statusCode = 201;
+  console.log(req.url, req.method);
 });
 
 server.listen(3000, console.log("Server Started!..."));

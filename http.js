8dad22a78dc373570438e5f8111e72fbc398acfb
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-04-30 21:46:40 +0500

    updated

--- http.js
+++ http.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// console.log("hello");
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/course" && req.method == "GET") {
+  }
+  res.statusCode = 201;
+  res.write("User Created Successfully!");
+  console.log(req.url, req.method);
+
+  res.end();
+});
+
+server.listen(3000, console.log("Server Started!..."));
